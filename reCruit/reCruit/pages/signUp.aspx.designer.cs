1eff115736b9119e7d5fa3185a91507b0c50d5c0
l H <hl_bupt@163.com>
2020-06-10 11:04:37 +0800

    20000220

--- reCruit/reCruit/pages/signUp.aspx.designer.cs
+++ reCruit/reCruit/pages/signUp.aspx.designer.cs
@@ -7,13 +7,11 @@
 // </自动生成>
 //------------------------------------------------------------------------------
 
-namespace reCruit.pages
-{
-
-
-    public partial class WebForm2
-    {
-
+namespace reCruit.pages {
+    
+    
+    public partial class WebForm2 {
+        
         /// <summary>
         /// form1 控件。
         /// </summary>
@@ -22,5 +20,14 @@ namespace reCruit.pages
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Label1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
